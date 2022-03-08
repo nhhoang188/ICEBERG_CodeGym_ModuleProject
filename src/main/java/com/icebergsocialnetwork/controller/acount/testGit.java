6f017b80b6279a8743e3e7b38fa4496136cc7e94
hoangnh <hoangnh@nganluong.vn>
2022-03-08 21:24:50 +0700

    conflic

--- src/main/java/com/icebergsocialnetwork/controller/acount/testGit.java
+++ src/main/java/com/icebergsocialnetwork/controller/acount/testGit.java
@@ -2,7 +2,7 @@ package com.icebergsocialnetwork.controller.acount;
 
 public class testGit {
     public static void main(String[] args) {
-        System.out.println("conflic");
-        System.out.println("code a tu");
+        System.out.println("conflic dasdas");
+        System.out.println("code a asdasda");
     }
 }
